ca73a7538a9a2cafcc2329f5e98899054c80f46c
prans <recoverfrancis@yahoo.com>
2026-03-23 01:05:59 +0800

    implement SB Admin bootstrap

--- Hangarin/hangarinproj/apps.py
+++ Hangarin/hangarinproj/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
+from django.views.generic import TemplateView
 
 
 class HangarinprojConfig(AppConfig):
     name = 'hangarinproj'
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AircraftListView(TemplateView):
+    template_name = "aircraft_list.html"
+
+class AircraftCreateView(TemplateView):
+    template_name = "aircraft_form.html"
